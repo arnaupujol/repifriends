fc758383c816ba2b5d82751eff17c33204fe4406
Mikel-Maj <61182741+Mikel-Maj@users.noreply.github.com>
2022-09-26 12:28:02 +0200

    Catalogue function adition. Indeces are sorted unlike the python version. There is no geopandas or any geopandaslike pckage included, so the geopandas format is not added.
    Tryals commented. Will be delated in the final version.

--- epifriends.R
+++ epifriends.R
@@ -10,16 +10,17 @@ datasin <- read.csv("mock_data_sin.csv")
 dataclus <- read.csv("mock_data_clustered.csv")
 
 position_rand <- datarand[3:4]
+test <- datarand[5]
 
 typeof(position_rand)
 
 #Prouebas
-dim(datarand)[2]
-indecesp <- list()
-indecesp[0] <- c()
-position_rand[2:4,] <- position_rand[1,]
-query <- nn2(position_rand, position_rand[1,], 4)
-query$nn.idx[length(query$nn.idx)]
+# dim(datarand)[2]
+# indecesp <- list()
+# indecesp[0] <- c()
+# position_rand[2:4,] <- position_rand[1,]
+# query <- nn2(position_rand, position_rand[1,], 4)
+# query$nn.idx[length(query$nn.idx)]
 
 #Functions
 
@@ -138,15 +139,144 @@ dbscan <- function(positions, link_d, min_neighbours = 2){
 }
 
 #Pruebas
-position_rand
-last_cluster_id = 0
-indeces_friends <- ind[[1]]
-cluster_id = integer(dim(position_rand)[1])
-cluster_id[ind[[1]]]
-cluster_id_friends <- cluster_id[indeces_friends]
-unique_cluster_ids <- unique(cluster_id_friends)
-length(unique_cluster_ids)
-cluster_id[indeces_friends] = last_cluster_id + 1
-last_cluster_id = last_cluster_id + 1
+# position_rand
+# last_cluster_id = 0
+# indeces_friends <- ind[[1]]
+# cluster_id = integer(dim(position_rand)[1])
+# cluster_id[ind[[1]]]
+# cluster_id_friends <- cluster_id[indeces_friends]
+# unique_cluster_ids <- unique(cluster_id_friends)
+# length(unique_cluster_ids)
+# cluster_id[indeces_friends] = last_cluster_id + 1
+# last_cluster_id = last_cluster_id + 1
 
 db <- dbscan(position_rand, 0.05 ,2)
+
+#Catalogue
+
+catalogue <- function(positions, test_result, link_d, cluster_id = NULL,
+                      min_neighbours = 2, max_p = 1, min_pos = 2, min_total = 2,
+                      min_pr = 0){
+  # This method runs the DBSCAN algorithm (if cluster_id is None) and obtains the mean
+  # positivity rate (PR) of each cluster extended with the non-infected cases
+  # closer than the link_d.
+  # 
+  # Parameters:
+  #   -----------
+  # positions: list
+  #   A list with the position parameters we want to query with shape (n,2),
+  #   where n is the number of positions
+  # test_result: list
+  #   A list with the test results (0 or 1)
+  # link_d: float
+  #   The linking distance to connect cases
+  # cluster_id: list
+  #   A list with the cluster ids of the positive cases
+  # min_neighbours: int
+  #   Minium number of neighbours in the radius < link_d needed to link cases
+  #   as friends
+  # max_p: float
+  #   Maximum value of the p-value to consider the cluster detection
+  # min_pos: int
+  #   Threshold of minimum number of positive cases in clusters applied
+  # min_total: int
+  #   Threshold of minimum number of cases in clusters applied
+  # min_pr: float
+  #   Threshold of minimum positivity rate in clusters applied
+  # 
+  # Returns:
+  #   --------
+  # cluster_id: list
+  #   List of the cluster IDs of each position, with 0 for those
+  #   without a cluster.
+  # mean_pr_fof: list
+  #   Mean PR corresponding to cluster_id
+  # pval_fof: list
+  #   P-value corresponding to cluster_id
+  # epifriends_catalogue: geopandas.DataFrame
+  #   Catalogue of the epifriends clusters and their main characteristics
+  
+  #Define positions of positive cases
+  positive_positions <- positions[test_result == 1,]
+  #Computing cluster_id if needed
+  if(is.null(cluster_id)){
+    cluster_id = dbscan(positive_positions, link_d, min_neighbours = min_neighbours)
+  }
+  #Define total number of positive cases
+  total_positives = sum(test_result)
+  #Define total number of cases
+  total_n = dim(test_result)[1]
+  #Initialising mean PR and p-value for the positive cases in clusters
+  mean_pr_cluster <- numeric(length(cluster_id))                       #!!!!!!!!!!!!!
+  pval_cluster <- rep(1, length(cluster_id))                           #!!!!!!!!!!!!!
+  #EpiFRIenDs cluster catalogue
+  epifriends_catalogue <- vector(mode='list', length=9)
+  names(epifriends_catalogue) <- c("id",                  #EpiFRIenDs id
+                                   "mean_position_pos",   #Mean position of positive cases
+                                   "mean_position_all",   #Mean position of all cases
+                                   "mean_pr",             #Positivity rate
+                                   "positives",           #Number of positive cases
+                                   "negatives",           #Number of negative cases
+                                   "total",               #Total number of positions
+                                   "indeces",             #Indeces of all positions
+                                   "p"                    #p-value of detection
+                                   )
+  next_id <- 1
+  sort_unici <- sort(unique(cluster_id[cluster_id>0]))
+  for(i in 1:length(sort_unici)){
+    #get all indeces with this cluster id
+    cluster_id_indeces <- which(cluster_id == sort_unici[i])
+    #for all these indeces, get list of friends from all positions
+    all_friends_indeces <- find_indeces(positive_positions[cluster_id_indeces,], link_d, positions)
+    #get unique values of such indeces
+    total_friends_indeces <- sort(unique(unlist(all_friends_indeces)))
+    #get positivity rate from all the unique indeces
+    mean_pr <- mean(test_result[total_friends_indeces,])
+    npos <- sum(test_result[total_friends_indeces,])
+    ntotal <- length(total_friends_indeces)
+    pval <- 1 - pbinom(npos - 1, ntotal, total_positives/total_n)
+    #setting EpiFRIenDs catalogue
+    if(pval < max_p && npos >= min_pos && ntotal >= min_total && mean_pr >= min_pr){
+      epifriends_catalogue[['id']] <- append(epifriends_catalogue[['id']], next_id)
+      cluster_id[cluster_id_indeces] <- next_id
+      next_id = next_id + 1
+      
+      mean_pr_cluster[cluster_id_indeces] <- mean_pr
+      pval_cluster[cluster_id_indeces] <- pval
+      
+      mean_pos <- colMeans(positive_positions[cluster_id_indeces,])
+      epifriends_catalogue[["mean_position_pos"]] <- append(epifriends_catalogue[["mean_position_pos"]], list(mean_pos))
+      mean_pos_ext <-  colMeans(positions[total_friends_indeces,])
+      epifriends_catalogue[["mean_position_all"]] <- append(epifriends_catalogue[["mean_position_all"]], list(mean_pos_ext))
+      epifriends_catalogue[["mean_pr"]] <- append(epifriends_catalogue[["mean_pr"]], mean_pr)
+      epifriends_catalogue[["positives"]] <- append(epifriends_catalogue[["positives"]], as.integer(npos))
+      epifriends_catalogue[["negatives"]] <- append(epifriends_catalogue[["negatives"]], as.integer(ntotal - npos))
+      epifriends_catalogue[["total"]] <- append(epifriends_catalogue[["total"]], as.integer(ntotal))
+      epifriends_catalogue[["indeces"]] <- append(epifriends_catalogue[["indeces"]], list(total_friends_indeces))
+      epifriends_catalogue[["p"]] <- append(epifriends_catalogue[["p"]], pval)
+    }else{
+      cluster_id[cluster_id_indeces] <- 0
+    }
+  }
+  #Creation of list of return variables
+  return <- list(cluster_id, mean_pr_cluster, pval_cluster, epifriends_catalogue)
+  names(return) <- c("cluster_id", "mean_pr_cluster", "pval_cluster", "epifriends_catalogue")
+  return(return)
+}
+
+cat <- catalogue(position_rand, test, 0.05)
+
+#Pruebas
+# positive_positions <- position_rand[test == 1,]
+# cluster_id = dbscan(positive_positions, 0.01, min_neighbours = 2)
+# total_positives = sum(test)
+# total_n = dim(test)[1]
+# epifriends_catalogue <- vector(mode='list', length=9)
+# names(epifriends_catalogue) <- c("id", "mean_position_pos", "mean_position_all", "mean_pr", "positives", "negatives", "total", "indeces", "p")
+# cluster_id_indeces = which(cluster_id == sort_unici[1])
+# all_friends_indeces = find_indeces(positive_positions[cluster_id_indeces,], 0.05, position_rand)
+# total_friends_indeces <- unique(unlist(all_friends_indeces))
+# mean_pr <- mean(test[total_friends_indeces,])
+# npos <- sum(test[total_friends_indeces,])
+# total <- length(total_friends_indeces)
+append(c(c(1,2)), c(c(2,3)))
